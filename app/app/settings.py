926c6eafdc62e70f0e65265431f5f4470105a5fa
İrfan <irfanbykara@gmail.com>
2022-03-21 13:26:42 +0300

    'ready'

--- app/app/settings.py
+++ app/app/settings.py
@@ -63,7 +63,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
