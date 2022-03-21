37e164d90500f26f56ba7b49cec81f409d940407
İrfan <irfanbykara@gmail.com>
2022-03-21 13:55:22 +0300

    'Hello_World'

--- app/core/urls.py
+++ app/core/urls.py
@@ -2,13 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 from . import views
-from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
-from django.conf.urls import url
-from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('',views.home, name='home'),
-
 ]
